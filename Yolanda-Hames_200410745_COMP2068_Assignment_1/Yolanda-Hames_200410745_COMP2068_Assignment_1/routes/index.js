a943572a51329bb763f47546980395d0a0a02995
Yhames1978 <54914229+Yhames1978@users.noreply.github.com>
2020-06-24 12:30:08 -0400

    Added routes in the index.js to all the new pug pages, and a hef link in the layout

--- Yolanda-Hames_200410745_COMP2068_Assignment_1/Yolanda-Hames_200410745_COMP2068_Assignment_1/routes/index.js
+++ Yolanda-Hames_200410745_COMP2068_Assignment_1/Yolanda-Hames_200410745_COMP2068_Assignment_1/routes/index.js
@@ -4,7 +4,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    var name = "Yolanda Hames Webites"
+
+    res.render('index', { name: name });
 });
+
+router.get('/aboutMe/', function (req, res) {
+    var name = "About Yolanda Hames"
+
+    res.render('aboutMe', { name: name });
+});
+router.get('/projects/', function (req, res) {
+    var name = "All Projects From Georgian College"
+
+    res.render('projects', { name: name });
+});
+router.get('/services/', function (req, res) {
+    var name = "Services"
+
+    res.render('services', { name: name });
+});
+router.get('/contactMe/', function (req, res) {
+    var name = "Contact Information"
+
+    res.render('contactMe', { name: name });
+});
+
 
 module.exports = router;
